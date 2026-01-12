78bc8b57dbc0d7fa4a8172641cb72c30210cf674
automacaofatuaras <automacaodefaturas@gmail.com>
2026-01-12 09:23:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3395,7 +3395,7 @@ export default function App() {
         <header className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4 sticky top-0 z-40 bg-slate-100 dark:bg-slate-900 py-2">
             {!['global', 'rateios'].includes(activeTab) ? (
             <div className="flex gap-2 w-full md:w-auto items-center flex-wrap">
-                <PeriodSelector filter={filter} setFilter={setFilter} years={[2024, 2025]} />
+                <PeriodSelector filter={filter} setFilter={setFilter} years={[2025, 2026, 2027]} />
                 <HierarchicalSelect 
                     value={globalUnitFilter} 
                     onChange={setGlobalUnitFilter} 
@@ -3410,8 +3410,8 @@ export default function App() {
             <div className="flex gap-2 self-end md:self-auto"></div>
         </header>
         
-        {activeTab === 'global' && <GlobalComponent transactions={transactions} filter={filter} setFilter={setFilter} years={[2024, 2025]} />}
-        {activeTab === 'rateios' && <RateiosComponent transactions={transactions} filter={filter} setFilter={setFilter} years={[2024, 2025]} segmentsList={segments} />}
+        {activeTab === 'global' && <GlobalComponent transactions={transactions} filter={filter} setFilter={setFilter} years={[2025, 2026, 2027]} />}
+        {activeTab === 'rateios' && <RateiosComponent transactions={transactions} filter={filter} setFilter={setFilter} years={[2025, 2026, 2027]} segmentsList={segments} />}
         
         {activeTab === 'dashboard' && (
           <div className="space-y-6 animate-in fade-in duration-500">
